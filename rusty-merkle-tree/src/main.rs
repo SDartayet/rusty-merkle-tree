c6ee10bed50e915ec8f061c5659ba8f4e3ed0db1
SDartayet <sofiadartayet@gmail.com>
2025-03-18 16:25:03 -0300

    Refactored struct to make implementation easier

--- rusty-merkle-tree/src/main.rs
+++ rusty-merkle-tree/src/main.rs
@@ -1,20 +1,18 @@
 //Hashvalues are of type u64. The type aliasing to HashValue is there to help with code readability.
+
+use std::hash::{DefaultHasher, Hash, Hasher};
 struct HashValue(u64);
 
 
 //Struct definition for branch nodes of the Merkle Tree. 
 //Boxes are needed for the children due to it being a recursive data structure
-struct MerkleTreeBranch {
+struct MerkleTreeBranch{
     hash: HashValue,
-    leftChild: Box<MerkleTreeBranch>,
-    rightChild: Box<MerkleTreeBranch>,
+    leftChild: Box<Option<MerkleTreeBranch>>,
+    rightChild: Box<Option<MerkleTreeBranch>>,
 }
-
-//A Merkle Tree node can either be a leaf (and only have a hash), or a branch.
-enum MerkleTreeNode {
-    Leaf(HashValue),
-    Branch(MerkleTreeBranch),
-}
-
 fn main() {}
+
+
+
 
